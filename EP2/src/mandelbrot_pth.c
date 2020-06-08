d225ca189ce412adb6f04d8d53ee54837a0c663a
Lucy <lucy.omena@live.com>
2020-06-08 15:55:21 -0300

    Add enunciado do EP2 - arrumando pasta

---
+++ EP2/src/mandelbrot_pth.c
@@ -0,0 +1,297 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <pthread.h>
+#include <time.h>
+#include <sys/time.h>
+
+struct timer_info {
+    clock_t c_start;
+    clock_t c_end;
+    struct timespec t_start;
+    struct timespec t_end;
+    struct timeval v_start;
+    struct timeval v_end;
+};
+
+struct timer_info timer, timerAloc;
+
+int NTHREADS;
+
+double c_x_min;
+double c_x_max;
+double c_y_min;
+double c_y_max;
+
+double pixel_width;
+double pixel_height;
+
+int iteration_max = 200;
+
+int image_size;
+unsigned char **image_buffer;
+
+int i_x_max;
+int i_y_max;
+int image_buffer_size;
+
+int gradient_size = 16;
+int colors[17][3] = {
+                        {66, 30, 15},
+                        {25, 7, 26},
+                        {9, 1, 47},
+                        {4, 4, 73},
+                        {0, 7, 100},
+                        {12, 44, 138},
+                        {24, 82, 177},
+                        {57, 125, 209},
+                        {134, 181, 229},
+                        {211, 236, 248},
+                        {241, 233, 191},
+                        {248, 201, 95},
+                        {255, 170, 0},
+                        {204, 128, 0},
+                        {153, 87, 0},
+                        {106, 52, 3},
+                        {16, 16, 16},
+                    };
+
+
+
+void allocate_image_buffer(){
+    int rgb_size = 3;
+    image_buffer = (unsigned char **) malloc(sizeof(unsigned char *) * image_buffer_size);
+
+    for(int i = 0; i < image_buffer_size; i++){
+        image_buffer[i] = (unsigned char *) malloc(sizeof(unsigned char) * rgb_size);
+    };
+};
+
+void init(int argc, char *argv[]){
+    if(argc < 6){
+        printf("usage: ./mandelbrot_pth c_x_min c_x_max c_y_min c_y_max image_size\n");
+        printf("examples with image_size = 11500:\n");
+        printf("    Full Picture:         ./mandelbrot_pth -2.5 1.5 -2.0 2.0 11500\n");
+        printf("    Seahorse Valley:      ./mandelbrot_pth -0.8 -0.7 0.05 0.15 11500\n");
+        printf("    Elephant Valley:      ./mandelbrot_pth 0.175 0.375 -0.1 0.1 11500\n");
+        printf("    Triple Spiral Valley: ./mandelbrot_pth -0.188 -0.012 0.554 0.754 11500\n");
+        exit(0);
+    }
+    else{
+        sscanf(argv[1], "%lf", &c_x_min);
+        sscanf(argv[2], "%lf", &c_x_max);
+        sscanf(argv[3], "%lf", &c_y_min);
+        sscanf(argv[4], "%lf", &c_y_max);
+        sscanf(argv[5], "%d", &image_size);
+        sscanf(argv[6], "%d", &NTHREADS);
+
+        i_x_max           = image_size;
+        i_y_max           = image_size;
+        image_buffer_size = image_size * image_size;
+
+        pixel_width       = (c_x_max - c_x_min) / i_x_max;
+        pixel_height      = (c_y_max - c_y_min) / i_y_max;
+    };
+};
+
+void update_rgb_buffer(int iteration, int x, int y){
+    int color;
+
+    if(iteration == iteration_max){
+        image_buffer[(i_y_max * y) + x][0] = colors[gradient_size][0];
+        image_buffer[(i_y_max * y) + x][1] = colors[gradient_size][1];
+        image_buffer[(i_y_max * y) + x][2] = colors[gradient_size][2];
+    }
+    else{
+        color = iteration % gradient_size;
+
+        image_buffer[(i_y_max * y) + x][0] = colors[color][0];
+        image_buffer[(i_y_max * y) + x][1] = colors[color][1];
+        image_buffer[(i_y_max * y) + x][2] = colors[color][2];
+    };
+};
+
+void write_to_file(){
+    FILE * file;
+    char * filename               = "output_pth.ppm";
+    char * comment                = "# ";
+
+    int max_color_component_value = 255;
+
+    file = fopen(filename,"wb");
+
+    fprintf(file, "P6\n %s\n %d\n %d\n %d\n", comment,
+            i_x_max, i_y_max, max_color_component_value);
+
+    for(int i = 0; i < image_buffer_size; i++){
+        fwrite(image_buffer[i], 1 , 3, file);
+    };
+
+    fclose(file);
+};
+
+/*PTHREAD FUNCTIONS*/
+/*
+void work(int i,int j){
+    double z_x;
+    double z_y;
+    double z_x_squared;
+    double z_y_squared;
+    double escape_radius_squared = 4;
+    int iteration;
+
+    double c_x;
+    double c_y;
+
+    c_y = c_y_min + j * pixel_height;
+
+    if(fabs(c_y) < pixel_height / 2){
+        c_y = 0.0;
+    };
+    c_x         = c_x_min + i * pixel_width;
+
+    z_x         = 0.0;
+    z_y         = 0.0;
+
+    z_x_squared = 0.0;
+    z_y_squared = 0.0;
+    for(iteration = 0;
+        iteration < iteration_max && \
+        ((z_x_squared + z_y_squared) < escape_radius_squared);
+        iteration++){
+        z_y         = 2 * z_x * z_y + c_y;
+        z_x         = z_x_squared - z_y_squared + c_x;
+
+        z_x_squared = z_x * z_x;
+        z_y_squared = z_y * z_y;
+    }
+    update_rgb_buffer(iteration, i, j);
+}*/
+
+void work(int j){
+    double z_x;
+    double z_y;
+    double z_x_squared;
+    double z_y_squared;
+    double escape_radius_squared = 4;
+    int i_x;
+
+    int iteration;
+
+    double c_x;
+    double c_y;
+
+    c_y = c_y_min + j * pixel_height;
+    if(fabs(c_y) < pixel_height / 2){
+        c_y = 0.0;
+    };
+    for(i_x = 0; i_x < i_x_max; i_x++){
+        c_x         = c_x_min + i_x * pixel_width;
+
+        z_x         = 0.0;
+        z_y         = 0.0;
+
+        z_x_squared = 0.0;
+        z_y_squared = 0.0;
+        for(iteration = 0;
+            iteration < iteration_max && \
+            ((z_x_squared + z_y_squared) < escape_radius_squared);
+            iteration++){
+            z_y         = 2 * z_x * z_y + c_y;
+            z_x         = z_x_squared - z_y_squared + c_x;
+
+            z_x_squared = z_x * z_x;
+            z_y_squared = z_y * z_y;
+        }
+        update_rgb_buffer(iteration, i_x, j);
+    }
+}
+
+static pthread_mutex_t mutex=PTHREAD_MUTEX_INITIALIZER;
+//int i_x=0;
+int i_y=0;
+/*
+void* task(void* arg){
+    int i,j;
+    for(;;){
+        pthread_mutex_lock(&mutex);
+        if(i_y==i_y_max){
+            pthread_mutex_unlock(&mutex);
+            return NULL;
+        }
+        i=i_x;
+        j=i_y;
+        (i_x)++;
+        if(i_x==i_x_max){
+            (i_y)++;
+            i_x=0;
+        }
+        pthread_mutex_unlock(&mutex);
+        work(i,j);
+    }
+}*/
+
+void* task(void* arg){
+    int j;
+    for(;;){
+        pthread_mutex_lock(&mutex);
+        if(i_y==i_y_max){
+            pthread_mutex_unlock(&mutex);
+            return NULL;
+        }
+        j=i_y++;
+        pthread_mutex_unlock(&mutex);
+        work(j);
+    }
+}
+
+
+void create_threads(){
+    pthread_t* threads;
+    threads=(pthread_t* )malloc(NTHREADS*sizeof(pthread_t));
+    for(int i=0;i<NTHREADS;i++){
+        pthread_create(&threads[i],NULL,task,NULL);
+    }
+    for(int i=0;i<NTHREADS;i++){
+        pthread_join(threads[i], NULL);
+    }
+}
+
+/*PTHREAD FUNCTIONS*/
+
+int main(int argc, char *argv[]){
+    timerAloc.c_start = clock();
+    clock_gettime(CLOCK_MONOTONIC, &timerAloc.t_start);
+    gettimeofday(&timerAloc.v_start, NULL);
+
+    init(argc, argv);
+
+    allocate_image_buffer();
+
+    timer.c_start = clock();
+    clock_gettime(CLOCK_MONOTONIC, &timer.t_start);
+    gettimeofday(&timer.v_start, NULL);
+
+    create_threads();
+
+    timer.c_end = clock();
+    clock_gettime(CLOCK_MONOTONIC, &timer.t_end);
+    gettimeofday(&timer.v_end, NULL);
+
+    timerAloc.c_end = clock();
+    clock_gettime(CLOCK_MONOTONIC, &timerAloc.t_end);
+    gettimeofday(&timerAloc.v_end, NULL);
+
+
+    printf("%f",
+        (double) (timer.t_end.tv_sec - timer.t_start.tv_sec) +
+        (double) (timer.t_end.tv_nsec - timer.t_start.tv_nsec) / 1000000000.0);
+    
+    write_to_file();
+    printf (",");
+    printf("%f",
+        (double) (timerAloc.t_end.tv_sec - timerAloc.t_start.tv_sec) +
+        (double) (timerAloc.t_end.tv_nsec - timerAloc.t_start.tv_nsec) / 1000000000.0);
+    printf ("\n");
+    return 0;
+};
